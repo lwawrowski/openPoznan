d33b5d755e9bc42b3df29bf438660be14c27d3ad
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-07-19 13:48:47 +0200

    new col names method, new function name, place for loop which will check if there is any multi polygon (working on it)

--- R/GetParish.R
+++ R/GetParish.R
@@ -2,7 +2,8 @@
 
 #Parafie Calosc
 
-getParish <- function (coords = F) {
+parish <- function (coords = F) {
+  
   
   #Wstepna analiza
   
@@ -15,21 +16,31 @@ getParish <- function (coords = F) {
   Parish_basic_info <- data.frame(cbind(Parish_features$id,
                                         Parish_features$properties$name))
   
-  colnames(Parish_basic_info)[(names(Parish_basic_info) == "X1")] <- "ID"
-  colnames(Parish_basic_info)[(names(Parish_basic_info) == "X2")] <- "Parish_Name"
-  
-  
+  colnames(Parish_basic_info) <- c("ID",
+                                   "Parish_Name")
   
   Parish_coord <- Parish_features$geometry$coordinates
   
   Parish_coord_2d <- map(Parish_coord, drop)
   
+  # for (i in 1:nrow(Parish_features)){
+  #   
+  #    if (is.list(Parish_coord[[i]]) == F) {
+  #    
+  #    } else {
+  #      
+  #      print("Pętla działa, tu pisz kod")
+  #    }
+  # }
+  
   Parish_coord_df <- map(Parish_coord_2d, as.data.frame)
   
   Parish_coord_id <- map2_df(Parish_coord_df,Parish_features$id, ~mutate(.x, id=.y))
   
-  colnames(Parish_coord_id)[(names(Parish_coord_id) == "V1")] <- "Longitude"
-  colnames(Parish_coord_id)[(names(Parish_coord_id) == "V2")] <- "Latitude"
+  colnames(Parish_coord_id) <- c("Longitude",
+                                 "Latitude",
+                                 "ID")
+  
   
   if (coords == T) {
     result <- list(Parishes=Parish_basic_info,
@@ -43,6 +54,6 @@ getParish <- function (coords = F) {
 
 #examples
 
-Parish_basic <- getParish()
+parish_basic <- parish()
 
-Parish_with_coord <- getParish(TRUE)
+parish_with_coord <- parish(TRUE)
