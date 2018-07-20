cdd48307d6b60b5d20c81a976042c6f6190e627e
KonradJadwizyc <konradjadwizyc@gmail.com>
2018-07-20 15:56:05 +0200

    getEstate_new

--- R/getEstate.R
+++ R/getEstate.R
@@ -1,6 +1,11 @@
-getEstate <- function(arg = TRUE) {
+getEstate <- function(coords = T) {
+# wczytanie danych wyborów do rad osiedli 
+  
+  #wstępna analiza 
 e <- fromJSON("http://www.poznan.pl/featureserver/featureserver.cgi/wybory_ro_lokale/")
 estate <- e$features
+
+ #Oczyszczenie danych z niepotrzebnych informacji + nazwanie
 estate_coord <- data.frame(matrix(unlist(e$features$geometry$coordinates),
                                   nrow = nrow(e$features), byrow = T))
 colnames(estate_coord)[(names(estate_coord)=="X1")] <- "Longitude"
@@ -14,25 +19,20 @@ estate_basic_info <- data.frame(cbind(estate$id,
                                       estate$properties$przystosowanie,
                                       estate$properties$nr_lokalu,
                                       estate$properties$nr_obwodu))
-
-colnames(estate_basic_info)[(names(estate_basic_info)== "X1")] <- "ID"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X2")] <- "Type"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X3")] <- "Address"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X4")] <- "Residence"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X5")] <- "Electoral_District"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X6")] <- "AFDP"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X7")] <- "Venue_nr"
-colnames(estate_basic_info)[(names(estate_basic_info)== "X8")] <- "District_nr"
-
+colnames(estate_basic_info)<-c("ID","Type","Address","Residence","Electoral_District","AFDP","Venue_nr","District_nr")
 
 # złączenie wszystkich kolumn
 
 estate_final <- cbind(estate_basic_info, estate_coord)
-if(arg == TRUE){
-  +    result <- TRUE
-  +  } else {
-+    result <- FALSE
+
+if(coords == F){
+      result <- list(District=estate_basic_info,
+                      Coords = estate_coord)
+    } else {
+    return(estate_basic_info)
+      
+    }
 }
-+ 
-+  return(ticket_final)
-+}
+
+
+
