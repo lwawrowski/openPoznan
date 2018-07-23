b662a10d3fd600acc830b5d281053300dcb14fc4
KonradJadwizyc <konradjadwizyc@gmail.com>
2018-07-23 14:31:10 +0200

    getDydactic_Paths

---
+++ R/getDydactic_Paths.R
@@ -0,0 +1,31 @@
+
+getDydactic_Paths <- function(Coord = F){
+  
+  # szlaki dydaktyczne
+  
+  
+  d <- fromJSON("http://www.poznan.pl/mim/plan/map_service.html?mtype=tourism&co=dydactic_paths")
+  dpaths <- d$features
+  
+  trail_basic_info <- data.frame(cbind(dpaths$id,
+                                       dpaths$properties$name,
+                                       dpaths$properties$length,
+                                       dpaths$properties$desc))
+  
+  colnames(trail_basic_info)<-c("ID","Name","Lenght","Description")
+  
+  # z??czenie wszystkich kolumn
+  
+  dpaths <- d$features
+  
+  dcoord <- dpaths$geometry$coordinates
+  
+  dcoord_df <- map(dcoord, as.data.frame)
+  
+  dcoord_id <- map2_df(dcoord_df, dpaths$id, ~mutate(.x, id=.y))
+  if(Coord == T){
+    reuslt <- dcoord_id
+  } else {
+    return(trail_basic_info)
+  }
+}
