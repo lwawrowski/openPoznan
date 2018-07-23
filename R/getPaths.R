0bf58478d2474f773ae0bfecf7f11704d6cf77db
KonradJadwizyc <konradjadwizyc@gmail.com>
2018-07-23 14:29:47 +0200

    getPaths

---
+++ R/getPaths.R
@@ -0,0 +1,28 @@
+
+getPaths <- function(Coord = F){
+  # szlaki turystyczne 
+  
+  t <- fromJSON("http://www.poznan.pl/mim/plan/map_service.html?mtype=tourism&co=trails")
+  paths <- t$features
+  
+  paths_basic_info <- data.frame(cbind(paths$id,
+                                       paths$properties$name,
+                                       paths$properties$length,
+                                       paths$properties$desc))
+  
+  colnames(paths_basic_info)<-c("ID","Name","Lenght","Description")
+  
+  # z??czenie wszystkich kolumn
+  
+  pathscoord <- paths$geometry$coordinates
+  
+  pathscoord_df <- map(pathscoord, as.data.frame)
+  
+  pathscoord_id <- map2_df(pathscoord_df, paths$id, ~mutate(.x, id=.y))
+  
+  if(Coord == T){
+    reuslt <- pathscoord_id
+  } else {
+    return(paths_basic_info)
+  }
+}
