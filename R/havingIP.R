f7cb450573baf47bd306b4664d08a7e84965e1a1
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-07-24 10:50:32 +0200

    Function that checks if we got connection to internet. From: https://stackoverflow.com/questions/5076593/how-to-determine-if-you-have-an-internet-connection-in-r

---
+++ R/havingIP.R
@@ -0,0 +1,11 @@
+
+
+havingIP <- function() {
+  if (.Platform$OS.type == "windows") {
+    ipmessage <- system("ipconfig", intern = TRUE)
+  } else {
+    ipmessage <- system("ifconfig", intern = TRUE)
+  }
+  validIP <- "((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.]){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+  any(grep(validIP, ipmessage))
+}
