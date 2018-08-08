887910e3015937e9043b1f7ecfe3375b8bf72fba
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-08-08 11:59:53 +0200

    finished documentation.

--- R/GetTVM.R
+++ R/GetTVM.R
@@ -7,23 +7,17 @@
 #' @import jsonlite, textclean
 #' @format 
 #' \describe{
-#' \item{number}{character; bike number.}
-#' \item{bike_type}{character; bike type.}
-#' \item{lock_types}{character; lock types.}
-#' \item{id}{factor; station id.}
-#' \item{uid}{factor; uid.}
-#' \item{lat}{factor; latitude.}
-#' \item{lng}{factor; longitude}
-#' \item{name}{factor; name.}
-#' \item{bikes}{factor; bikes.}
-#' \item{bike_racks}{factor; bike racks.}
-#' \item{free_racks}{factor; free racks.}
-#' \item{bike_types}{factor; bike types.}
+#' \item{ID}{factor; ID of TVM.}
+#' \item{TVM}{factor; Name of TVM.}
+#' \item{PEKA}{factor; Shows if TVM accepts PEKA card.}
+#' \item{Store_Card}{factor; Shows if TVM accepts store card.}
+#' \item{TVM_Order}{factor; Order of TVM.}
+#' \item{TVM_Description }{factor; Description where you can find this TVM..}
+#' \item{Longitude}{numeric; Longitude of TVM.}
+#' \item{Latitude}{numeric; Latitude of TVM.}
 #' }
 #' @examples
 #' tvm()
-
-
 
 
 tvm <- function () {
