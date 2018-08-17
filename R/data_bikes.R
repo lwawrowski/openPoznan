827193644f33a53b94ae9ec773ba64f8c8cd84d8
Kamil Nowak <kamilnowakwagrowiec@gmail.com>
2018-08-17 12:04:07 +0200

    updated

---
+++ R/data_bikes.R
@@ -0,0 +1,28 @@
+#' data_bikes Function
+#'
+#' This function load data about bikes.
+#' @keywords keyword
+#' @export
+#' @details Details of usage 
+#' @format 
+#' \describe{
+#' \item{number}{character; bike number.}
+#' \item{bike_type}{character; bike type.}
+#' \item{lock_types}{character; lock types.}
+#' \item{id}{factor; station id.}
+#' \item{uid}{factor; uid.}
+#' \item{lat}{factor; latitude.}
+#' \item{lng}{factor; longitude.}
+#' \item{name}{factor; name.}
+#' \item{bikes}{factor; bikes.}
+#' \item{bike_racks}{factor; bike racks.}
+#' \item{free_racks}{factor; free racks.}
+#' \item{bike_types}{factor; bike types.}
+#' \item{date}{factor; date.}
+#' }
+#' @examples
+#' data_bikes()
+data_bikes <- function(){
+load("data/bikes.RData")
+  return(data_table_final)
+}
