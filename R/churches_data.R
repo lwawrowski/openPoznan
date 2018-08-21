48728c250078f67b0739577ba566707216f6bf06
WojciechDopieralski <wojciech.dopieralski@op.pl>
2018-08-21 14:10:53 +0200

    Dodano funkcję wczytującą Churches data, zmieniono nazwę pliku.

---
+++ R/churches_data.R
@@ -0,0 +1,12 @@
+#' churches_data  Function
+#'
+#' This function load data about churches in Poznan.
+#' @format 
+#' \describe{
+#' \item{Parish_Name.y}{character; Name of certain Church.}
+#' \item{Longitude}{numeric; Longitude of church.}
+#' \item{Latitude}{numeric; Latitude of church.}
+#' }
+#' @examples
+#' Churches <- churches_data()
+"churches_data"
